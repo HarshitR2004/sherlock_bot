9795d9a0346a15fd295f58ea8909dedf7cf8f4cb
HarshitR2004 <harshithranjan6971@gmail.com>
2025-03-10 11:04:31 +0530

    Update sherlock_bot.py

--- backend/sherlock_bot.py
+++ backend/sherlock_bot.py
@@ -6,7 +6,8 @@ from sentence_transformers import SentenceTransformer
 class SherlockBot:
     def __init__(self, api_key, chroma_path):
         """Initialize SherlockBot with necessary components"""
-        self.embedding_model = SentenceTransformer('backend\all-MiniLM-L6-v2') 
+        model_path = os.path.join("backend", "all-MiniLM-L6-v2")
+        self.embedding_model = SentenceTransformer(model_path) 
         self.chroma_client = chromadb.PersistentClient(path=chroma_path)
         
         try:
